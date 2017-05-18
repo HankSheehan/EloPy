aeb06c44db5d4716eb5f30a56e1bcc10b1ef4c98
HANK H SHEEHAN <hank.sheehan@ahold.com>
2017-05-18 16:24:19 -0400

    Created a addResult function. Started the functionality.

--- elopy.py
+++ elopy.py
@@ -3,6 +3,7 @@ Created 5-18-17
 All of the classes for EloPy. The users should only interact with the Implementation class
 @author - Hank Hang Kai Sheehan
 """
+
 class Implementation:
     """
     A class that represents an implementation of the Elo Rating System
@@ -35,6 +36,16 @@ class Implementation:
                 return True
         return False
 
+    def addResults(self, name1, name2):
+        """
+        Should be called after a game is played.
+        @param name1 - name of the first player.
+        @param name2 - name of the second player.
+        """
+        expected1 = ( 1+10**( ( R2-R1 )/400 ) ) ** -1
+        expected2 = ( 1+10**( ( R1-R2 )/400 ) ) ** -1
+        rating1 = #new rating formula
+        rating2 = #new rating forula
 
 class _Player:
     """
