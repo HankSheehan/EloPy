b68d992ad45afe0d9ec3d09b17693a20ecae4a53
Ming Fong <mingfong07@hotmail.com>
2020-03-02 22:30:03 -0800

    Update to Python 3

--- test.py
+++ test.py
@@ -5,20 +5,20 @@ i = Implementation()
 i.addPlayer("Hank")
 i.addPlayer("Bill",rating=900)
 
-print i.getPlayerRating("Hank"), i.getPlayerRating("Bill")
+print(i.getPlayerRating("Hank"), i.getPlayerRating("Bill"))
 
 i.recordMatch("Hank","Bill",winner="Hank")
 
-print i.getRatingList()
+print(i.getRatingList())
 
 i.recordMatch("Hank","Bill",winner="Bill")
 
-print i.getRatingList()
+print(i.getRatingList())
 
 i.recordMatch("Hank","Bill",draw=True)
 
-print i.getRatingList()
+print(i.getRatingList())
 
 i.removePlayer("Hank")
 
-print i.getRatingList()
+print(i.getRatingList())
